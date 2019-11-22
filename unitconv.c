b60266efc0132fe820f35ba4d7ce600c8e37b1da
wyatt8740 <wyatt8740@users.noreply.github.com>
2019-11-22 00:52:58 -0500

    units dropdown for input

--- unitconv.c
+++ unitconv.c
@@ -16,14 +16,20 @@ int display_height=0;
 int display_width=0;
 GtkLabel *resultLabel;
 GtkEntry *inputEntry;
+GtkComboBoxText *inLabel;
+int lengthMult=0; /* mm, cm, m multiplication factor */
 
 /* allocates and populates a string for using as a label. */
 /* this result will need to be freed sometime after each call. */
 gchar *parseEntry(const gchar *inputStr)
 {
   static const char DECIMAL_FORMAT_STRING[]="%.1f ft (%.2f\")";
-  double resultInch=strtod(inputStr,NULL) * 0.393701;
-  double resultFoot=(strtod(inputStr,NULL) * 0.393701 )/12.0;
+  /* convert to meters */
+  
+  /* meters to feet */
+  double resultInch=strtod(inputStr,NULL) * (1.0/lengthMult) * 39.3701;
+/*  double resultFoot=(strtod(inputStr,NULL) * (1.0/lengthMult) * 39.3701 )/12.0;*/
+  double resultFoot=resultInch / 12.0;
   /* find how much we need to allocate */
   size_t neededChars=snprintf(NULL,0,DECIMAL_FORMAT_STRING,resultFoot,resultInch);
   gchar *output=g_malloc((gsize)neededChars); /* normal malloc() probably is the same here */
@@ -91,6 +97,30 @@ input_delete_after (GtkEditable* edit, gchar *text,
   g_free(output);
 }
 
+void input_unit(GtkComboBox *widget, gpointer data)
+{
+  (void)data;
+  char *str=gtk_combo_box_text_get_active_text((GtkComboBoxText*)widget);
+  if(g_strcmp0(str,"mm") == 0)
+  {
+    lengthMult=1000;
+  }
+  else if(g_strcmp0(str,"cm") == 0)
+  {
+    lengthMult=100;
+  }
+  else
+  {
+    lengthMult=1;
+  }
+  const gchar* content = gtk_entry_get_text(GTK_ENTRY((GtkEditable*)inputEntry));
+  gchar* output = parseEntry( content);
+  gtk_label_set_text(resultLabel,output);
+/*    gtk_editable_set_position(edit, cursor_pos);*/
+  g_free(output);
+  
+}
+
 static void activate(GtkApplication* applet, gpointer user_data)
 {
   GtkWidget *window;
@@ -102,7 +132,20 @@ static void activate(GtkApplication* applet, gpointer user_data)
   GtkWidget *vbox=gtk_box_new (GTK_ORIENTATION_VERTICAL, 0); /* 0 spacing */
   GtkWidget *hboxInLine=gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
   GtkWidget *hboxOutLine=gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
-  GtkLabel *inLabel=(GtkLabel *)gtk_label_new("cm");
+/*  GtkLabel *inLabel=(GtkLabel *)gtk_label_new("cm"); */
+
+  /* inLabel is global */
+  inLabel=(GtkComboBoxText*)gtk_combo_box_text_new();
+  gtk_combo_box_text_append(inLabel, 0, "mm");
+  gtk_combo_box_text_append(inLabel, 0, "cm");
+  gtk_combo_box_text_append(inLabel, 0, "m");
+  /* text variant doesn't have its own 'set_active' func, so we do a cast */
+  gtk_combo_box_set_active((GtkComboBox*)inLabel,1);
+  /* defaults to cm so lengthMult must be 100 */
+  lengthMult=100;
+
+  g_signal_connect(inLabel, "changed", G_CALLBACK(input_unit), NULL );
+
 /*  GtkLabel *outLabelInch=(GtkLabel *)gtk_label_new("in");*/
 /*  GtkLabel *outLabelFoot=(GtkLabel *)gtk_label_new("ft)");*/
   gtk_container_add (GTK_CONTAINER (window), vbox);
